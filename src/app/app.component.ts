a85e0f3c5c3050127b11d2b799550832450fc421
Rina Bedini <rina@e-soft.al>
2024-12-26 17:20:11 +0100

    Add configuration files, styles, and layout components; remove unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
-import { Component,OnInit } from '@angular/core';
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
-import { NavbarComponent } from "./shared/navbar/navbar.component";
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, NavbarComponent ,MatSidenavModule, MatToolbarModule, MatListModule, MatIconModule, MatButtonModule, MatDividerModule],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  isMobile = false;
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
-  ngOnInit(): void {
-
-  }
+export class AppComponent {
+  title = 'Modernize Angular Admin Template';
 }
